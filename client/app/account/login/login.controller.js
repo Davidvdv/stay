10f6bac8350b1ee970fc604a2339e12fdf83ca3a
Carson Bruce <carson.bruce@solnet.co.nz>
2016-03-11 10:56:58 +1300

    centered login

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -18,6 +18,9 @@ class LoginController {
 
 
     $timeout(() => {
+
+      this.loginInit = true;
+
       if(this.user.email) {
         angular.element(document.getElementById('password-input')).focus();
       }
