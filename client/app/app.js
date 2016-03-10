a7ae5204b093d828073cb0f403ec1a2bfadfaf25
Carson Bruce <carson.bruce@solnet.co.nz>
2016-03-10 16:41:34 +1300

    solnet blue

--- client/app/app.js
+++ client/app/app.js
@@ -13,9 +13,34 @@ angular.module('stayApp', [
   'ui.router',
   'validation.match'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $mdThemingProvider) {
 
     $urlRouterProvider.otherwise('/timesheet/');
 
     $locationProvider.html5Mode(true);
+
+    $mdThemingProvider.definePalette('amazingPaletteName', {
+      '50': '0E589E',
+      '100': '0E589E',
+      '200': '0E589E',
+      '300': '0E589E',
+      '400': '0E589E',
+      '500': '0E589E',
+      '600': '0E589E',
+      '700': '0E589E',
+      '800': '0E589E',
+      '900': '0E589E',
+      'A100': '0E589E',
+      'A200': '0E589E',
+      'A400': '0E589E',
+      'A700': '0E589E',
+      'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                          // on this palette should be dark or light
+      'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+        '200', '300', '400', 'A100'],
+      'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+    $mdThemingProvider.theme('default')
+      .primaryPalette('amazingPaletteName')
+
   });
