ba504ca482e80bc70233502d6bd69eb4d1edce57
Carson Bruce <carson.bruce@solnet.co.nz>
2016-03-11 14:27:36 +1300

    promise deps

--- server/components/project-service/project.service.local.js
+++ server/components/project-service/project.service.local.js
@@ -1,7 +1,6 @@
 
 
 import moment from 'moment';
-import Promise from 'bluebird';
 import _ from 'lodash';
 import * as projectYats from './project.service.yats.js';
 import * as timesheetService from '../timesheet-service/timesheet.service.local.js';
@@ -12,7 +11,6 @@ import * as timesheetYats from '../timesheet-service/timesheet.service.yats.js';
 export function searchProjects(user, clientId){
   return timesheetService.getDummyTimesheet(user)
     .then(editTimesheet => {
-
       //https://yats.solnetsolutions.co.nz/timesheets/update_row_and_clear_ajax/509405?timesheet_id=65355
       return projectYats.searchProjects(user, clientId, editTimesheet);
     });
